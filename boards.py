9764556b4b0ec87162c1dd125aedc88de3e02248
Camilla Alves <arcamilla42@gmail.com>
2026-06-03 14:35:03 -0300

    feat: added support for repeating player and custom moves

--- boards.py
+++ boards.py
@@ -3,54 +3,49 @@ from typing import Iterator
 from generators import build_generator, ChoiceOptions, safe_next
 
 SAVE_FOLDER = Path("./out")
+BACKGROUND_COLOR = (255, 255, 255, 255)
+TEAM_COLORS = [
+    (0, 0, 0, 255),
+    (255, 0, 0, 255),
+    (0, 255, 0, 255),
+    (0, 0, 255, 255),
+    (255, 255, 0, 255),
+    (0, 255, 255, 255),
+    (255, 0, 255, 255),
+    (255, 128, 0, 255),
+]
 
-ColourData = tuple[int, int, int, int]
 CoordData = tuple[int, int]
 
 
-class Board:
-    KNIGHT_MOVES = (
-        (-2, -1),
-        (-2, 1),
-        (-1, -2),
-        (-1, 2),
-        (1, -2),
-        (1, 2),
-        (2, -1),
-        (2, 1),
-    )
+class Piece:
+    def __init__(self, value: int, moves: tuple[tuple[int, int], ...]) -> None:
+        self.value = value
+        self.moves = moves
 
+
+class Board:
     def __init__(
-        self, height: int, width: int, colours_amount: int, choice: ChoiceOptions
+        self, height: int, width: int, piece_orders: list[Piece], choice: ChoiceOptions
     ) -> None:
         self.height = height
         self.width = width
         self.cell_amount = height * width
-        self.colours_amount = colours_amount
+        self.piece_orders = piece_orders
+        self.colours_amount = max([p.value for p in piece_orders])
         self.board = [[0 for _ in range(width)] for _ in range(height)]
         self.ordered_cells: list[CoordData] = []
         self.choice = choice
         self.generators: list[Iterator[CoordData]] = [
-            build_generator(self) for _ in range(colours_amount)
+            build_generator(self) for _ in range(self.colours_amount)
         ]
-
-    def clean(self) -> None:
-        for row in self.board:
-            for i in range(len(row)):
-                row[i] = 0
-
-    def restart(self, colours_amount: int) -> None:
-        self.clean()
-        self.colours_amount = colours_amount
-        self.generators = [build_generator(self) for _ in range(colours_amount)]
 
     def is_inside(self, y: int, x: int) -> bool:
         return 0 <= y < self.height and 0 <= x < self.width
 
-    def is_coord_safe(self, coord: CoordData, turn: int) -> bool:
-        piece = turn + 1
+    def is_coord_safe(self, coord: CoordData, piece: Piece) -> bool:
         y, x = coord
-        for dy, dx in self.KNIGHT_MOVES:
+        for dy, dx in piece.moves:
             ay = y + dy
             ax = x + dx
             if not self.is_inside(ay, ax):
@@ -60,33 +55,34 @@ class Board:
                 return False
         return True
 
-    def solve(self, colours: list[ColourData], background_color: ColourData) -> None:
+    def solve(self) -> None:
         turn = 0
         while True:
-            generator = self.generators[turn]
+            piece = self.piece_orders[turn]
+            generator = self.generators[piece.value - 1]
             coord = safe_next(generator)
             if not coord:
                 break
             y, x = coord
             cell = self.board[y][x]
-            if cell != 0 or not self.is_coord_safe(coord, turn):
+            if cell != 0 or not self.is_coord_safe(coord, piece):
                 continue
+            self.board[y][x] = piece.value
             turn += 1
-            self.board[y][x] = turn
-            if turn >= self.colours_amount:
+            if turn >= len(self.piece_orders):
                 turn = 0
-        self.render(colours, background_color)
+        self.render()
 
-    def render(self, colours: list[ColourData], background_color: ColourData) -> None:
+    def render(self) -> None:
         from PIL import Image
 
-        img = Image.new("RGBA", (self.width, self.height), background_color)
+        img = Image.new("RGBA", (self.width, self.height), BACKGROUND_COLOR)
         for y in range(self.height):
             for x in range(self.width):
                 piece = self.board[y][x]
                 if piece == 0:
                     continue
-                img.putpixel((x, y), colours[piece - 1])
+                img.putpixel((x, y), TEAM_COLORS[piece - 1])
         file_amount = sum(1 for item in SAVE_FOLDER.iterdir() if item.is_file())
         output_image = SAVE_FOLDER / f"output_{file_amount}.png"
         img.save(output_image)
--- main.py
+++ main.py
@@ -1,26 +1,38 @@
-from boards import Board, ChoiceOptions
+from boards import Board, ChoiceOptions, Piece
 
 
 WIDTH = 1000
 HEIGHT = 1000
-BACKGROUND_COLOR = (255, 255, 255, 255)
-TEAM_COLORS = [
-    (0, 0, 0, 255),
-    (255, 0, 0, 255),
-    (0, 255, 0, 255),
-    (0, 0, 255, 255),
-    (255, 255, 0, 255),
-    (0, 255, 255, 255),
-    (255, 0, 255, 255),
-    (255, 128, 0, 255),
-]
+KNIGHT_MOVES = (
+    (-2, -1),
+    (-2, 1),
+    (-1, -2),
+    (-1, 2),
+    (1, -2),
+    (1, 2),
+    (2, -1),
+    (2, 1),
+)
+TRI_KNIGHT_MOVES = (
+    (-3, -1),
+    (-3, 1),
+    (-1, -3),
+    (-1, 3),
+    (1, -3),
+    (1, 3),
+    (3, -1),
+    (3, 1),
+)
 
 
 def main() -> None:
-    board = Board(HEIGHT, WIDTH, 2, ChoiceOptions.SPIRAL_DIAGONAL)
-    for a in range(2, 9):
-        board.restart(a)
-        board.solve(TEAM_COLORS, BACKGROUND_COLOR)
+    board = Board(
+        HEIGHT,
+        WIDTH,
+        [Piece(n, KNIGHT_MOVES) for n in range(1, 3)],
+        ChoiceOptions.SPIRAL,
+    )
+    board.solve()
 
 
 if __name__ == "__main__":
